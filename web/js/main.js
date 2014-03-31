c5befad29bab72be2b1f0dcff1d99a7d59be25cc
elliciachen <elliica@kik.com>
2014-03-31 14:11:46 -0400

    add pickUser

--- web/js/main.js
+++ web/js/main.js
@@ -3,7 +3,7 @@
 	App.populator('intro', function (page) {
 		//body
 		$(page)
-    		.find('.app-button')
+    		.find('.app-button.link')
     		.on('click', function () {
 	      		kik.getUser(function (user) {
 				    if ( !user ) {
@@ -22,11 +22,34 @@
 
 	App.populator('home', function (page) {
 		// put stuff here
+
 	});
 
 	App.populator('page2', function (page) {
 		// put stuff here
+		$(page)
+    		.find('.app-button.userPick')
+    		.on('click', function () {
+	      		kik.pickUsers(function (users) {
+				    if ( !users ) {
+				        App.load('page2');
+				    } else {
+				        users.forEach(function (user) {
+				            typeof user.username;  // 'string'
+				            typeof user.fullName;  // 'string'
+				            typeof user.firstName; // 'string'
+				            typeof user.lastName;  // 'string'
+				            typeof user.pic;       // 'string'
+				            typeof user.thumbnail; // 'string'
+				        });
+				    }
+				});
+    	});
 	});
+
+	App.populator('page2_1', function (page) {
+		// body...
+	})
 
 	App.populator('page3', function (page) {
 		// body...
